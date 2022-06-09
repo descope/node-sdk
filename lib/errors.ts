dac47065ce36d681db580c6229018513ad078c4b
Bar Saar <bar@descope.com>
2022-06-09 19:36:29 +0300

    format

--- lib/errors.ts
+++ lib/errors.ts
@@ -1,12 +1,12 @@
 import { requestConfig } from './shared';
 
 export class RequestError extends Error {
-	request: requestConfig;
+  request: requestConfig;
 
-	constructor(request: requestConfig, error?: Error, message?: string) {
-		super(error?.message || message);
-		this.request = request;
-	}
+  constructor(request: requestConfig, error?: Error, message?: string) {
+    super(error?.message || message);
+    this.request = request;
+  }
 }
 
 export class JWTError extends Error {}
--- lib/testutils/mocks.ts
+++ lib/testutils/mocks.ts
@@ -1,30 +1,30 @@
 const jwtTokenValid =
-	'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzM4NCIsImtpZCI6IjBhZDk5ODY5ZjJkNGU1N2YzZjcxYzY4MzAwYmE4NGZhIn0.eyJleHAiOjE5ODEzOTgxMTF9.MHSHryNl0oU3ZBjWc0pFIBKlXHcXU0vcoO3PpRg8MIQ8M14k4sTsUqJfxXCTbxh24YKE6w0XFBh9B4L7vjIY7iVZPM44LXNEzUFyyX3m6eN_iAavGKPKdKnao2ayNeu1';
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzM4NCIsImtpZCI6IjBhZDk5ODY5ZjJkNGU1N2YzZjcxYzY4MzAwYmE4NGZhIn0.eyJleHAiOjE5ODEzOTgxMTF9.MHSHryNl0oU3ZBjWc0pFIBKlXHcXU0vcoO3PpRg8MIQ8M14k4sTsUqJfxXCTbxh24YKE6w0XFBh9B4L7vjIY7iVZPM44LXNEzUFyyX3m6eN_iAavGKPKdKnao2ayNeu1';
 const jwtTokenExpired =
-	'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzM4NCIsImtpZCI6IjBhZDk5ODY5ZjJkNGU1N2YzZjcxYzY4MzAwYmE4NGZhIn0.eyJleHAiOjExODEzOTgxMTF9.Qbi7klMrWKSM2z89AtMyDk_lRYnxxz0WApEO5iPikEcAzemmJyR_7b1IvHVxR4uQgCZrH46anUD0aTtPG7k3PpMjP2o2pDHWgY0mWlxW0lDlMqkrvZtBPC7qa_NJTHFl';
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzM4NCIsImtpZCI6IjBhZDk5ODY5ZjJkNGU1N2YzZjcxYzY4MzAwYmE4NGZhIn0.eyJleHAiOjExODEzOTgxMTF9.Qbi7klMrWKSM2z89AtMyDk_lRYnxxz0WApEO5iPikEcAzemmJyR_7b1IvHVxR4uQgCZrH46anUD0aTtPG7k3PpMjP2o2pDHWgY0mWlxW0lDlMqkrvZtBPC7qa_NJTHFl';
 
 export class Mock {
-	projectID: string = 'test';
+  projectID: string = 'test';
 
-	JWT = class {
-		static valid: string = jwtTokenValid;
+  JWT = class {
+    static valid: string = jwtTokenValid;
 
-		static expired: string = jwtTokenExpired;
-	};
+    static expired: string = jwtTokenExpired;
+  };
 
-	PublicKeys = class {
-		static valid = {
-			crv: 'P-384',
-			d: 'FfTHqwIAM3OMj808FlAL59OkwdXnfmc8FAXtTqyKnfu023kXHtDrAjEwMEBnOC3O',
-			key_ops: ['sign'],
-			kty: 'EC',
-			x: 'c9ZzWUHmgUpCiDMpxaIhPxORaFqMx_HB6DQUmFM0M1GFCdxoaZwAPv2KONgoaRxZ',
-			y: 'zTt0paDnsE98Sd7erCVvLWLGGnGcjbOVy5C3m6AI116hUV5JeFAspBe_uDTnAfBD',
-			alg: 'ES384',
-			use: 'sig',
-			kid: '0ad99869f2d4e57f3f71c68300ba84fa',
-		};
-	};
+  PublicKeys = class {
+    static valid = {
+      crv: 'P-384',
+      d: 'FfTHqwIAM3OMj808FlAL59OkwdXnfmc8FAXtTqyKnfu023kXHtDrAjEwMEBnOC3O',
+      key_ops: ['sign'],
+      kty: 'EC',
+      x: 'c9ZzWUHmgUpCiDMpxaIhPxORaFqMx_HB6DQUmFM0M1GFCdxoaZwAPv2KONgoaRxZ',
+      y: 'zTt0paDnsE98Sd7erCVvLWLGGnGcjbOVy5C3m6AI116hUV5JeFAspBe_uDTnAfBD',
+      alg: 'ES384',
+      use: 'sig',
+      kid: '0ad99869f2d4e57f3f71c68300ba84fa',
+    };
+  };
 }
 
 const mocks = new Mock();
