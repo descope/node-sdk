aa506b328f65cab3189623acd7a6d412dff7dc2a
Nir Gur Arie <nir@descope.com>
2022-07-13 11:22:52 +0300

    reusing web-js

--- examples/es6/src/index.ts
+++ examples/es6/src/index.ts
@@ -1,127 +1,135 @@
-import express, { Request, Response, NextFunction } from 'express';
-import { DescopeClient, DeliveryMethod, OAuthProvider } from '@descope/node-sdk';
-import * as fs from 'fs';
-import * as https from 'https';
+import express, { Request, Response, NextFunction } from 'express'
+import DescopeClient from '../../../dist/index1.js'
+import type { DeliveryMethod, OAuthProvider } from '../../../dist/index1.js'
+import * as fs from 'fs'
+import * as https from 'https'
+import path from 'path';
 
-const app = express();
-const port = 443;
-const clientAuth = new DescopeClient({ projectId: process.env.DESCOPE_PROJECT_ID || "", baseURL: 'http://localhost:8191/v1/' });
-var options = {
-  key: fs.readFileSync('../../server.key'),
-  cert: fs.readFileSync('../../server.crt'),
-};
+const app = express()
+const port = 443
+
+const options = {
+  key: fs.readFileSync(path.resolve('../server.key')),
+  cert: fs.readFileSync(path.resolve('../server.crt')),
+}
+
+const clientAuth = {
+  auth: DescopeClient({ projectId: process.env.DESCOPE_PROJECT_ID || '', logger: console }),
+}
 
 const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const cookies = parseCookies(req);
-    const out = await clientAuth.Auth.ValidateSession(cookies['DS'], cookies['DSR']);
+    const cookies = parseCookies(req)
+    const out = await clientAuth.auth.ValidateSession(cookies['DS'], cookies['DSR'])
     if (out?.cookies) {
-      res.set('Set-Cookie', out.cookies);
+      res.set('Set-Cookie', out.cookies)
     }
-    next();
+    next()
   } catch (e) {
     res.status(401).json({
       error: new Error('Unauthorized!'),
-    });
+    })
   }
-};
+}
 
 app.get('/otp/signup', async (req: Request, res: Response) => {
-  const { identifier, deliveryMethod } = getMethodAndIdentifier(req);
+  const { identifier, deliveryMethod } = getMethodAndIdentifier(req)
   try {
-    await clientAuth.Auth.SignUpOTP({ identifier, deliveryMethod });
-    res.sendStatus(200);
+    await clientAuth.auth.otp.signUp[deliveryMethod](identifier)
+    res.sendStatus(200)
   } catch (error) {
-    console.log(error);
-    res.sendStatus(401);
+    console.log(error)
+    res.sendStatus(401)
   }
-});
+})
 
 app.get('/otp/signin', async (req: Request, res: Response) => {
-  const { identifier, deliveryMethod } = getMethodAndIdentifier(req);
+  const { identifier, deliveryMethod } = getMethodAndIdentifier(req)
   try {
-    await clientAuth.Auth.SignInOTP({ identifier, deliveryMethod });
-    res.sendStatus(200);
+    await clientAuth.auth.otp.signIn[deliveryMethod](identifier)
+    res.sendStatus(200)
   } catch (error) {
-    console.log(error);
-    res.sendStatus(401);
+    console.log(error)
+    res.sendStatus(401)
   }
-});
+})
 
 app.get('/otp/verify', async (req: Request, res: Response) => {
-  const { identifier, deliveryMethod } = getMethodAndIdentifier(req);
-  const code = req.query.code as string;
+  const { identifier, deliveryMethod } = getMethodAndIdentifier(req)
+  const code = req.query.code as string
   try {
-    const out = await clientAuth.Auth.VerifyCode({ identifier, deliveryMethod, code });
-    if (out?.cookies) {
-      res.set('Set-Cookie', out.cookies);
+    const out = await clientAuth.auth.otp.verify[deliveryMethod](code, identifier )
+    if (out.data.jwts) {
+      res.set('Set-Cookie', out.data.jwts)
     }
-    res.sendStatus(200);
+    res.sendStatus(200)
   } catch (error) {
-    console.log(error);
-    res.sendStatus(401);
+    console.log(error)
+    res.sendStatus(401)
   }
-});
+})
 
-app.get('/oauth', async (req: Request, res: Response) => {
-  const provider = req.query.provider as OAuthProvider || OAuthProvider.facebook;
+app.get('/oauth', (req: Request, res: Response) => {
+  const provider: OAuthProvider = (req.query.provider as OAuthProvider)
   try {
-    const url = await clientAuth.Auth.StartOAuth(provider);
-    res.redirect(url);
+    const url =  clientAuth.auth.oauth.start[provider]()
+    res.redirect(url)
   } catch (error) {
-    console.log(error);
-    res.sendStatus(401);
+    console.log(error)
+    res.sendStatus(500)
   }
-});
+})
 
 app.get('/private', authMiddleware, (_unused: Request, res: Response) => {
-  res.sendStatus(200);
-});
+  res.sendStatus(200)
+})
 
 app.get('/logout', authMiddleware, async (req: Request, res: Response) => {
   try {
-    const cookies = parseCookies(req);
-    const out = await clientAuth.Auth.Logout(cookies['DS'], cookies['DSR']);
-    if (out?.cookies) {
-      res.set('Set-Cookie', out.cookies);
+    const cookies = parseCookies(req)
+    const out = await clientAuth.auth.logout(cookies['DS'])
+    if (out.data.jwts) {
+      res.set('Set-Cookie', out.data.jwts[0])
     }
-    res.sendStatus(200);
+    res.sendStatus(200)
   } catch (error) {
-    console.log(error);
-    res.sendStatus(401);
+    console.log(error)
+    res.sendStatus(401)
   }
-});
+})
 
 https.createServer(options, app).listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
 
-const getMethodAndIdentifier = (req: Request): { identifier: string, deliveryMethod: DeliveryMethod } => {
+const getMethodAndIdentifier = (
+  req: Request,
+): { identifier: string; deliveryMethod: DeliveryMethod } => {
   if (req.query.email) {
-    return { identifier: req.query.email as string, deliveryMethod: DeliveryMethod.email };
+    return { identifier: req.query.email as string, deliveryMethod: 'email' }
   }
   if (req.query.sms) {
-    return { identifier: req.query.sms as string, deliveryMethod: DeliveryMethod.SMS };
+    return { identifier: req.query.sms as string, deliveryMethod: 'sms' }
   }
   if (req.query.whatsapp) {
-    return { identifier: req.query.whatsapp as string, deliveryMethod: DeliveryMethod.whatsapp };
+    return { identifier: req.query.whatsapp as string, deliveryMethod: 'whatsapp' }
   }
-  return { identifier: '', deliveryMethod: DeliveryMethod.email };
-};
+  return { identifier: '', deliveryMethod: 'email' }
+}
 
 const parseCookies = (request: Request) => {
-  const list: { [key: string]: string } = {};
-  const cookieHeader = request.headers?.cookie;
-  if (!cookieHeader) return list;
+  const list: { [key: string]: string } = {}
+  const cookieHeader = request.headers?.cookie
+  if (!cookieHeader) return list
 
-  cookieHeader.split(`;`).forEach(function (cookie) {
-    let [name, ...rest] = cookie.split(`=`);
-    name = name?.trim();
-    if (!name) return;
-    const value = rest.join(`=`).trim();
-    if (!value) return;
-    list[name] = decodeURIComponent(value);
-  });
+  cookieHeader.split(`;`).forEach(function (cookie: string) {
+    let [name, ...rest] = cookie.split(`=`)
+    name = name?.trim()
+    if (!name) return
+    const value = rest.join(`=`).trim()
+    if (!value) return
+    list[name] = decodeURIComponent(value)
+  })
 
-  return list;
-};
+  return list
+}
--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,79 @@
-export { DescopeClient } from './api'
-export { JWTError, MissingArgumentError as ProjectIdMissingError } from './shared/errors'
-export { DeliveryMethod, Config, OAuthProvider } from './shared'
-export type { IConfig } from './shared'
+import createSdk from '@descope/web-js-sdk'
+import { KeyLike, jwtVerify, JWK, JWTHeaderParameters, importJWK } from 'jose'
+
+interface Token {
+  sub?: string
+  exp?: number
+  iss?: string
+}
+interface AuthenticationInfo {
+  token?: Token
+  cookies?: string[]
+}
+
+export type { DeliveryMethod, OAuthProvider } from '@descope/web-js-sdk'
+
+export default (...args: Parameters<typeof createSdk>) => {
+  const sdk = createSdk(...args)
+
+  const { projectId, logger } = args[0]
+
+  const keys: Record<string, KeyLike | Uint8Array> = {}
+
+  const fetchKeys = async () => {
+    const publicKeys: JWK[] =
+      (await sdk.httpClient.get(`keys/${projectId}`).then((resp) => resp.json())) || []
+    const kidJwksPairs = await Promise.all(
+      publicKeys.map(async (key) => [key.kid, await importJWK(key)]),
+    )
+
+    return kidJwksPairs.reduce(
+      (acc, [kid, jwk]) => (kid ? { ...acc, [kid.toString()]: jwk } : acc),
+      {},
+    )
+  }
+
+  return {
+    ...sdk,
+
+    async getKey(header: JWTHeaderParameters): Promise<KeyLike | Uint8Array> {
+      if (!header?.kid) throw Error('header.kid must not be empty')
+
+      if (keys[header.kid]) return keys[header.kid]
+
+      // do we need to fetch once or every time?
+      Object.assign(keys, await fetchKeys())
+
+      if (!keys[header.kid]) throw Error('failed to fetch matching key')
+
+      return keys[header.kid]
+    },
+
+    async validateToken(token: string): Promise<AuthenticationInfo> {
+      const res = await jwtVerify(token, this.getKey, { algorithms: ['ES384'] })
+
+      return { token: res.payload }
+    },
+
+    async ValidateSession(
+      sessionToken: string,
+      refreshToken: string,
+    ): Promise<AuthenticationInfo | undefined> {
+      if (!sessionToken) throw Error('session token must not be empty')
+
+      try {
+        return await this.validateToken(sessionToken)
+      } catch (error) {
+        try {
+          await this.validateToken(refreshToken)
+
+          return (await this.refresh(refreshToken)).data
+        } catch (refreshTokenErr) {
+          logger?.error('failed to validate refresh token', refreshTokenErr)
+
+          throw Error('could not validate tokens')
+        }
+      }
+    },
+  }
+}
