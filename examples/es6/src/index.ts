132b6ff114793ffc8ed56a51aaac775fb720362f
meirwah <meir@descope.com>
2022-07-28 09:31:17 +0300

    change to POST

--- examples/es6/src/index.ts
+++ examples/es6/src/index.ts
@@ -35,7 +35,7 @@ const authMiddleware = async (req: Request, res: Response, next: NextFunction) =
   }
 }
 
-app.get('/otp/signup', async (req: Request, res: Response) => {
+app.post('/otp/signup', async (req: Request, res: Response) => {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
   const { identifier, deliveryMethod } = getMethodAndIdentifier(req)
   try {
@@ -47,7 +47,7 @@ app.get('/otp/signup', async (req: Request, res: Response) => {
   }
 })
 
-app.get('/otp/signin', async (req: Request, res: Response) => {
+app.post('/otp/signin', async (req: Request, res: Response) => {
   const { identifier, deliveryMethod } = getMethodAndIdentifier(req)
   try {
     await clientAuth.auth.otp.signIn[deliveryMethod](identifier)
@@ -58,9 +58,9 @@ app.get('/otp/signin', async (req: Request, res: Response) => {
   }
 })
 
-app.get('/otp/verify', async (req: Request, res: Response) => {
+app.post('/otp/verify', async (req: Request, res: Response) => {
   const { identifier, deliveryMethod } = getMethodAndIdentifier(req)
-  const code = req.query.code as string
+  const code = req.body.code as string
   try {
     const out = await clientAuth.auth.otp.verify[deliveryMethod](code, identifier )
     if (out.data.jwts) {
@@ -73,8 +73,8 @@ app.get('/otp/verify', async (req: Request, res: Response) => {
   }
 })
 
-app.get('/oauth', (req: Request, res: Response) => {
-  const provider: OAuthProvider = (req.query.provider as OAuthProvider)
+app.post('/oauth', (req: Request, res: Response) => {
+  const provider: OAuthProvider = (req.body.provider as OAuthProvider)
   try {
     const url =  clientAuth.auth.oauth.start[provider]()
     res.redirect(url)
@@ -84,11 +84,11 @@ app.get('/oauth', (req: Request, res: Response) => {
   }
 })
 
-app.get('/private', authMiddleware, (_unused: Request, res: Response) => {
+app.post('/private', authMiddleware, (_unused: Request, res: Response) => {
   res.sendStatus(200)
 })
 
-app.get('/logout', authMiddleware, async (req: Request, res: Response) => {
+app.post('/logout', authMiddleware, async (req: Request, res: Response) => {
   try {
     const cookies = parseCookies(req)
     const out = await clientAuth.auth.logout(cookies['DS'])
@@ -109,14 +109,14 @@ https.createServer(options, app).listen(port, () => {
 const getMethodAndIdentifier = (
   req: Request,
 ): { identifier: string; deliveryMethod: DeliveryMethod } => {
-  if (req.query.email) {
-    return { identifier: req.query.email as string, deliveryMethod: 'email' }
+  if (req.body.email) {
+    return { identifier: req.body.email as string, deliveryMethod: 'email' }
   }
-  if (req.query.sms) {
-    return { identifier: req.query.sms as string, deliveryMethod: 'sms' }
+  if (req.body.sms) {
+    return { identifier: req.body.sms as string, deliveryMethod: 'sms' }
   }
-  if (req.query.whatsapp) {
-    return { identifier: req.query.whatsapp as string, deliveryMethod: 'whatsapp' }
+  if (req.body.whatsapp) {
+    return { identifier: req.body.whatsapp as string, deliveryMethod: 'whatsapp' }
   }
   return { identifier: '', deliveryMethod: 'email' }
 }
