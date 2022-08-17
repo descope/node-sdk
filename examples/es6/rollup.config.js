da76c413ba8b287b00b76c6e550b6c3b45235293
Bar Saar <bar@descope.com>
2022-08-17 15:45:49 +0300

    use es6 for the es6 example and fix docker (#31)

--- examples/es6/rollup.config.js
+++ examples/es6/rollup.config.js
@@ -4,15 +4,13 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json'
 
-const packageJson = require('./package.json');
-
 export default [
 	{
 		input: './src/index.ts',
 		output: [
 			{
 				file: 'dist/index.js',
-				format: 'cjs',
+				format: 'es',
 				sourcemap: true,
 			}
 		],
