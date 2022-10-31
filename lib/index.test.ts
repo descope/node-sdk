e1cea5c6de83d0f63ec5210063a6e3c090fc574a
dorsha <dorsha@descope.com>
2022-10-31 23:06:41 +0200

    Fix lint

--- lib/index.test.ts
+++ lib/index.test.ts
@@ -1,56 +1,56 @@
-import { JWTResponse, SdkResponse, ExchangeAccessKeyResponse } from '@descope/core-js-sdk'
-import { JWK, SignJWT, exportJWK, JWTHeaderParameters, generateKeyPair } from 'jose'
-import createSdk from '.'
-import { AuthenticationInfo } from './types'
+import { JWTResponse, SdkResponse, ExchangeAccessKeyResponse } from '@descope/core-js-sdk';
+import { JWK, SignJWT, exportJWK, JWTHeaderParameters, generateKeyPair } from 'jose';
+import createSdk from '.';
+import { AuthenticationInfo } from './types';
 import {
   refreshTokenCookieName,
   sessionTokenCookieName,
   authorizedTenantsClaimName,
   permissionsClaimName,
   rolesClaimName,
-} from './constants'
+} from './constants';
 
-let validToken: string
-let expiredToken: string
-let publicKeys: JWK
-let permAuthInfo: AuthenticationInfo
-let permTenantAuthInfo: AuthenticationInfo
+let validToken: string;
+let expiredToken: string;
+let publicKeys: JWK;
+let permAuthInfo: AuthenticationInfo;
+let permTenantAuthInfo: AuthenticationInfo;
 
 const logger = {
   log: jest.fn(),
   error: jest.fn(),
   debug: jest.fn(),
-}
+};
 
 const sdk = createSdk({
   projectId: 'project-id',
   logger,
-})
+});
 
 const get = (obj: Record<string, any>, path: string) =>
-  path.split('.').reduce((acc, part) => acc[part], obj) as Function
+  path.split('.').reduce((acc, part) => acc[part], obj) as Function;
 const generatePathFromKeys = (obj: Record<string, any>, path: string) =>
-  Object.keys(get(obj, path)).map((key) => `${path}.${key}`)
+  Object.keys(get(obj, path)).map((key) => `${path}.${key}`);
 
 describe('sdk', () => {
   beforeAll(async () => {
-    const { publicKey, privateKey } = await generateKeyPair('ES384')
+    const { publicKey, privateKey } = await generateKeyPair('ES384');
     validToken = await new SignJWT({})
       .setProtectedHeader({ alg: 'ES384', kid: '0ad99869f2d4e57f3f71c68300ba84fa' })
       .setIssuedAt()
       .setIssuer('project-id')
       .setExpirationTime(1981398111)
-      .sign(privateKey)
+      .sign(privateKey);
     expiredToken = await new SignJWT({})
       .setProtectedHeader({ alg: 'ES384', kid: '0ad99869f2d4e57f3f71c68300ba84fa' })
       .setIssuedAt(1181398100)
       .setIssuer('project-id')
       .setExpirationTime(1181398111)
-      .sign(privateKey)
+      .sign(privateKey);
     permAuthInfo = {
       jwt: 'jwt',
       token: { [permissionsClaimName]: ['foo', 'bar'], [rolesClaimName]: ['abc', 'xyz'] },
-    }
+    };
     permTenantAuthInfo = {
       jwt: 'jwt',
       token: {
@@ -58,176 +58,176 @@ describe('sdk', () => {
           kuku: { [permissionsClaimName]: ['foo', 'bar'], [rolesClaimName]: ['abc', 'xyz'] },
         },
       },
-    }
-    publicKeys = await exportJWK(publicKey)
-    publicKeys.alg = 'ES384'
-    publicKeys.kid = '0ad99869f2d4e57f3f71c68300ba84fa'
-    publicKeys.use = 'sig'
+    };
+    publicKeys = await exportJWK(publicKey);
+    publicKeys.alg = 'ES384';
+    publicKeys.kid = '0ad99869f2d4e57f3f71c68300ba84fa';
+    publicKeys.use = 'sig';
     jest
       .spyOn(sdk.httpClient, 'get')
-      .mockResolvedValue({ json: () => Promise.resolve([publicKeys]) } as Response)
-  })
+      .mockResolvedValue({ json: () => Promise.resolve([publicKeys]) } as Response);
+  });
   afterEach(() => {
-    jest.clearAllMocks()
-  })
+    jest.clearAllMocks();
+  });
   describe('validateToken', () => {
     it('should return the token payload when valid', async () => {
-      const resp = await sdk.validateJwt(validToken)
+      const resp = await sdk.validateJwt(validToken);
       expect(resp).toMatchObject({
         token: {
           exp: 1981398111,
           iss: 'project-id',
         },
-      })
-    })
+      });
+    });
 
     it('should reject with a proper error message when token expired', async () => {
       await expect(sdk.validateJwt(expiredToken)).rejects.toThrow(
         '"exp" claim timestamp check failed',
-      )
-    })
-  })
+      );
+    });
+  });
 
   describe('getKey', () => {
     it('should throw an error when key does not exist', async () => {
       await expect(sdk.getKey({ kid: 'unknown-key' } as JWTHeaderParameters)).rejects.toThrow(
         'failed to fetch matching key',
-      )
-    })
+      );
+    });
     it('should return the key from cache if exists', async () => {
       const newSdk = createSdk({
         projectId: 'project-id',
         logger,
-      })
+      });
 
       jest
         .spyOn(newSdk.httpClient, 'get')
-        .mockResolvedValue({ json: () => Promise.resolve([publicKeys]) } as Response)
+        .mockResolvedValue({ json: () => Promise.resolve([publicKeys]) } as Response);
 
-      await newSdk.getKey({ kid: publicKeys.kid } as JWTHeaderParameters)
-      await newSdk.getKey({ kid: publicKeys.kid } as JWTHeaderParameters)
+      await newSdk.getKey({ kid: publicKeys.kid } as JWTHeaderParameters);
+      await newSdk.getKey({ kid: publicKeys.kid } as JWTHeaderParameters);
 
-      expect(newSdk.httpClient.get).toHaveBeenCalledTimes(1)
-    })
+      expect(newSdk.httpClient.get).toHaveBeenCalledTimes(1);
+    });
 
     it('should throw an error when kid is empty', async () => {
       await expect(sdk.getKey({ kid: '' } as JWTHeaderParameters)).rejects.toThrow(
         'header.kid must not be empty',
-      )
-    })
-  })
+      );
+    });
+  });
 
   describe('ValidateSession', () => {
     it('should throw an error when both session and refresh tokens are empty', async () => {
       await expect(sdk.validateSession('', '')).rejects.toThrow(
         'both refresh token and session token are empty',
-      )
-    })
+      );
+    });
     it('should return the token when session token is valid', async () => {
       await expect(sdk.validateSession(validToken, '')).resolves.toMatchObject({
         token: { exp: 1981398111, iss: 'project-id' },
-      })
-    })
+      });
+    });
     it('should throw an error when session token expired and no refresh token', async () => {
       await expect(sdk.validateSession(expiredToken, '')).rejects.toThrow(
         'could not validate tokens',
-      )
-    })
+      );
+    });
     it('should throw an error when both refresh & session tokens expired', async () => {
       await expect(sdk.validateSession(expiredToken, expiredToken)).rejects.toThrow(
         'could not validate tokens',
-      )
-    })
+      );
+    });
     it('should refresh session token when it expired and refresh token is valid', async () => {
       const spyRefresh = jest.spyOn(sdk, 'refresh').mockResolvedValueOnce({
         ok: true,
         data: { sessionJwt: validToken },
-      } as SdkResponse<JWTResponse>)
+      } as SdkResponse<JWTResponse>);
 
       await expect(sdk.validateSession(expiredToken, validToken)).resolves.toHaveProperty(
         'jwt',
         validToken,
-      )
-      expect(spyRefresh).toHaveBeenCalledWith(validToken)
-    })
+      );
+      expect(spyRefresh).toHaveBeenCalledWith(validToken);
+    });
     it('should return the token when refresh token is valid', async () => {
       const spyRefresh = jest.spyOn(sdk, 'refresh').mockResolvedValueOnce({
         ok: true,
         data: { sessionJwt: validToken },
-      } as SdkResponse<JWTResponse>)
+      } as SdkResponse<JWTResponse>);
 
-      await expect(sdk.validateSession('', validToken)).resolves.toHaveProperty('jwt', validToken)
-      expect(spyRefresh).toHaveBeenCalledWith(validToken)
-    })
-  })
+      await expect(sdk.validateSession('', validToken)).resolves.toHaveProperty('jwt', validToken);
+      expect(spyRefresh).toHaveBeenCalledWith(validToken);
+    });
+  });
 
   describe('exchangeAccessKey', () => {
     it('should fail when the server call throws', async () => {
-      const spyExchange = jest.spyOn(sdk.accessKey, 'exchange').mockRejectedValueOnce('error')
-      await expect(sdk.exchangeAccessKey('key')).rejects.toThrow('could not exchange access key')
-      expect(spyExchange).toHaveBeenCalledWith('key')
-    })
+      const spyExchange = jest.spyOn(sdk.accessKey, 'exchange').mockRejectedValueOnce('error');
+      await expect(sdk.exchangeAccessKey('key')).rejects.toThrow('could not exchange access key');
+      expect(spyExchange).toHaveBeenCalledWith('key');
+    });
     it('should fail when getting an unexpected response from the server', async () => {
       const spyExchange = jest
         .spyOn(sdk.accessKey, 'exchange')
-        .mockResolvedValueOnce({ data: {} } as SdkResponse<ExchangeAccessKeyResponse>)
-      await expect(sdk.exchangeAccessKey('key')).rejects.toThrow('could not exchange access key')
-      expect(spyExchange).toHaveBeenCalledWith('key')
-    })
+        .mockResolvedValueOnce({ data: {} } as SdkResponse<ExchangeAccessKeyResponse>);
+      await expect(sdk.exchangeAccessKey('key')).rejects.toThrow('could not exchange access key');
+      expect(spyExchange).toHaveBeenCalledWith('key');
+    });
     it('should fail when the session token the server returns is invalid', async () => {
       const spyExchange = jest.spyOn(sdk.accessKey, 'exchange').mockResolvedValueOnce({
         data: { sessionJwt: expiredToken },
-      } as SdkResponse<ExchangeAccessKeyResponse>)
-      await expect(sdk.exchangeAccessKey('key')).rejects.toThrow('could not exchange access key')
-      expect(spyExchange).toHaveBeenCalledWith('key')
-    })
+      } as SdkResponse<ExchangeAccessKeyResponse>);
+      await expect(sdk.exchangeAccessKey('key')).rejects.toThrow('could not exchange access key');
+      expect(spyExchange).toHaveBeenCalledWith('key');
+    });
     it('should return the same session token it got from the server', async () => {
       const spyExchange = jest.spyOn(sdk.accessKey, 'exchange').mockResolvedValueOnce({
         data: { sessionJwt: validToken },
-      } as SdkResponse<ExchangeAccessKeyResponse>)
+      } as SdkResponse<ExchangeAccessKeyResponse>);
       const expected: AuthenticationInfo = {
         jwt: validToken,
         token: { exp: 1981398111, iss: 'project-id' },
-      }
-      await expect(sdk.exchangeAccessKey('key')).resolves.toMatchObject(expected)
-      expect(spyExchange).toHaveBeenCalledWith('key')
-    })
-  })
+      };
+      await expect(sdk.exchangeAccessKey('key')).resolves.toMatchObject(expected);
+      expect(spyExchange).toHaveBeenCalledWith('key');
+    });
+  });
 
   describe('validatePermissionsRoles', () => {
     it('should always succeed with empty requirements', () => {
-      expect(sdk.validatePermissions(permAuthInfo, [])).toStrictEqual(true)
-      expect(sdk.validateTenantPermissions(permTenantAuthInfo, 'kuku', [])).toStrictEqual(true)
-      expect(sdk.validateRoles(permAuthInfo, [])).toStrictEqual(true)
-      expect(sdk.validateTenantRoles(permTenantAuthInfo, 'kuku', [])).toStrictEqual(true)
-    })
+      expect(sdk.validatePermissions(permAuthInfo, [])).toStrictEqual(true);
+      expect(sdk.validateTenantPermissions(permTenantAuthInfo, 'kuku', [])).toStrictEqual(true);
+      expect(sdk.validateRoles(permAuthInfo, [])).toStrictEqual(true);
+      expect(sdk.validateTenantRoles(permTenantAuthInfo, 'kuku', [])).toStrictEqual(true);
+    });
     it('should succeed when requirements are met', () => {
-      expect(sdk.validatePermissions(permAuthInfo, ['foo'])).toStrictEqual(true)
+      expect(sdk.validatePermissions(permAuthInfo, ['foo'])).toStrictEqual(true);
       expect(
         sdk.validateTenantPermissions(permTenantAuthInfo, 'kuku', ['foo', 'bar']),
-      ).toStrictEqual(true)
-      expect(sdk.validateRoles(permAuthInfo, ['abc'])).toStrictEqual(true)
+      ).toStrictEqual(true);
+      expect(sdk.validateRoles(permAuthInfo, ['abc'])).toStrictEqual(true);
       expect(sdk.validateTenantRoles(permTenantAuthInfo, 'kuku', ['abc', 'xyz'])).toStrictEqual(
         true,
-      )
-    })
+      );
+    });
     it('should fail when wrong function is used', () => {
-      expect(sdk.validatePermissions(permTenantAuthInfo, ['foo'])).toStrictEqual(false)
-      expect(sdk.validateTenantPermissions(permAuthInfo, 'kuku', ['foo'])).toStrictEqual(false)
-      expect(sdk.validateRoles(permTenantAuthInfo, ['abc'])).toStrictEqual(false)
-      expect(sdk.validateTenantRoles(permAuthInfo, 'kuku', ['abc'])).toStrictEqual(false)
-    })
+      expect(sdk.validatePermissions(permTenantAuthInfo, ['foo'])).toStrictEqual(false);
+      expect(sdk.validateTenantPermissions(permAuthInfo, 'kuku', ['foo'])).toStrictEqual(false);
+      expect(sdk.validateRoles(permTenantAuthInfo, ['abc'])).toStrictEqual(false);
+      expect(sdk.validateTenantRoles(permAuthInfo, 'kuku', ['abc'])).toStrictEqual(false);
+    });
     it('should fail when requirements are not met', () => {
-      expect(sdk.validatePermissions(permAuthInfo, ['foo', 'bar', 'qux'])).toStrictEqual(false)
+      expect(sdk.validatePermissions(permAuthInfo, ['foo', 'bar', 'qux'])).toStrictEqual(false);
       expect(
         sdk.validateTenantPermissions(permTenantAuthInfo, 'kuku', ['foo', 'bar', 'qux']),
-      ).toStrictEqual(false)
-      expect(sdk.validateRoles(permAuthInfo, ['abc', 'xyz', 'tuv'])).toStrictEqual(false)
+      ).toStrictEqual(false);
+      expect(sdk.validateRoles(permAuthInfo, ['abc', 'xyz', 'tuv'])).toStrictEqual(false);
       expect(
         sdk.validateTenantRoles(permTenantAuthInfo, 'kuku', ['abc', 'xyz', 'tuv']),
-      ).toStrictEqual(false)
-    })
-  })
+      ).toStrictEqual(false);
+    });
+  });
 
   describe('withCookies', () => {
     describe('when no cookie', () => {
@@ -242,18 +242,18 @@ describe('sdk', () => {
         'webauthn.signIn.finish',
         'webauthn.signUp.finish',
         'refresh',
-      ]
+      ];
 
       it.each(paths)('should generate cookie from body jwt for %s', async (path) => {
-        const data = { sessionJwt: 'sessionJwt', refreshJwt: 'refreshJwt' }
+        const data = { sessionJwt: 'sessionJwt', refreshJwt: 'refreshJwt' };
         jest.spyOn(sdk.httpClient, 'post').mockResolvedValueOnce({
           ok: true,
           json: () => Promise.resolve(data),
-        } as Response)
+        } as Response);
         jest.spyOn(sdk.httpClient, 'get').mockResolvedValueOnce({
           ok: true,
           json: () => Promise.resolve(data),
-        } as Response)
+        } as Response);
 
         await expect(get(sdk, path)('1', '2', '3')).resolves.toEqual(
           expect.objectContaining({
@@ -265,17 +265,17 @@ describe('sdk', () => {
               ],
             },
           }),
-        )
-      })
+        );
+      });
 
       it.each(paths)('should generate jwt from cookie for %s', async (path) => {
-        const data = { sessionJwt: 'sessionJwt' }
-        const cookie = `${refreshTokenCookieName}=refreshJwt; Domain=; Max-Age=; Path=/; HttpOnly; SameSite=Strict`
+        const data = { sessionJwt: 'sessionJwt' };
+        const cookie = `${refreshTokenCookieName}=refreshJwt; Domain=; Max-Age=; Path=/; HttpOnly; SameSite=Strict`;
         jest.spyOn(sdk.httpClient, 'post').mockResolvedValueOnce({
           ok: true,
           json: () => Promise.resolve(data),
           headers: new Headers({ 'set-cookie': cookie }),
-        } as Response)
+        } as Response);
 
         await expect(get(sdk, path)('1', '2', '3')).resolves.toEqual(
           expect.objectContaining({
@@ -288,32 +288,32 @@ describe('sdk', () => {
               ],
             },
           }),
-        )
-      })
-    })
-  })
+        );
+      });
+    });
+  });
 
   describe('hooks', () => {
     it('should add descope headers to request', async () => {
-      jest.resetModules()
-      const coreSdk = jest.fn()
-      jest.doMock('@descope/core-js-sdk', () => coreSdk)
+      jest.resetModules();
+      const coreSdk = jest.fn();
+      jest.doMock('@descope/core-js-sdk', () => coreSdk);
       const createNodeSdk = require('.').default // eslint-disable-line
 
       createNodeSdk({
         projectId: 'project-id',
         logger,
-      })
+      });
 
-      const returnedConf = coreSdk.mock.calls[0][0].hooks.beforeRequest({})
+      const returnedConf = coreSdk.mock.calls[0][0].hooks.beforeRequest({});
 
       expect(returnedConf).toEqual({
         headers: {
           'x-descope-sdk-name': 'nodejs',
-          'x-descope-sdk-node-version': '16.18.0',
+          'x-descope-sdk-node-version': process?.versions?.node || '',
           'x-descope-sdk-version': 'one.two.three',
         },
-      })
-    })
-  })
-})
+      });
+    });
+  });
+});
