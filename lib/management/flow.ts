78eb1a22fc4a8c5c4df4b1e0f8e9186a17f33dda
Doron Sharon <dorsha@descope.com>
2024-01-22 23:34:37 +0200

    Add the ability to delete flows (#321)

--- lib/management/flow.ts
+++ lib/management/flow.ts
@@ -6,6 +6,10 @@ import { FlowResponse, FlowsResponse, Screen, Flow } from './types';
 const WithFlow = (sdk: CoreSdk, managementKey?: string) => ({
   list: (): Promise<SdkResponse<FlowsResponse>> =>
     transformResponse(sdk.httpClient.post(apiPaths.flow.list, {}, { token: managementKey })),
+  delete: (flowIds: string[]): Promise<SdkResponse<never>> =>
+    transformResponse(
+      sdk.httpClient.post(apiPaths.flow.delete, { ids: flowIds }, { token: managementKey }),
+    ),
   export: (flowId: string): Promise<SdkResponse<FlowResponse>> =>
     transformResponse(
       sdk.httpClient.post(apiPaths.flow.export, { flowId }, { token: managementKey }),
