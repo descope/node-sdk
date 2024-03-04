98129b12a9499afb89169fe71534d2b1b32bc251
GuyP <105154237+guyp-descope@users.noreply.github.com>
2024-03-04 16:26:32 +0200

    Update lib/management/accesskey.ts
    Co-authored-by: Gil Shapira <gshapira@gmail.com>

--- lib/management/accesskey.ts
+++ lib/management/accesskey.ts
@@ -19,7 +19,7 @@ const withAccessKey = (sdk: CoreSdk, managementKey?: string) => ({
    * @param roles Optional roles in the project. Does not apply for multi-tenants
    * @param keyTenants Optional associated tenants for this key and its roles for each.
    * @param userId Optional bind this access key to a specific user.
-   * @param customClaims Optional map of claims and their values that will be presented in the jwt
+   * @param customClaims Optional map of claims and their values that will be present in the JWT.
    * @returns A newly created key and its cleartext. Make sure to save the cleartext securely.
    */
   create: (
