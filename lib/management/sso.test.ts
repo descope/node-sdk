fdd1a6d036c74a5fb4b86794a18756358969c562
dorsha <dorsha@descope.com>
2025-03-15 17:14:00 +0200

    Multi SSO support

--- lib/management/sso.test.ts
+++ lib/management/sso.test.ts
@@ -72,6 +72,34 @@ describe('Management SSO', () => {
         data: {},
       });
     });
+
+    it('should send the correct request and receive correct response with sso id', async () => {
+      const httpResponse = {
+        ok: true,
+        json: () => {},
+        clone: () => ({
+          json: () => Promise.resolve({}),
+        }),
+        status: 200,
+      };
+      mockHttpClient.delete.mockResolvedValue(httpResponse);
+
+      const tenantId = 't1';
+      const ssoId = 'somessoid';
+      const resp = await management.sso.deleteSettings(tenantId, ssoId);
+
+      expect(mockHttpClient.delete).toHaveBeenCalledWith(apiPaths.sso.settings, {
+        queryParams: { tenantId, ssoId },
+        token: 'key',
+      });
+
+      expect(resp).toEqual({
+        code: 200,
+        ok: true,
+        response: httpResponse,
+        data: {},
+      });
+    });
   });
 
   describe('configureSettings', () => {
@@ -237,6 +265,55 @@ describe('Management SSO', () => {
         response: httpResponse,
       });
     });
+
+    it('should send the correct request and receive correct response with sso id', async () => {
+      const httpResponse = {
+        ok: true,
+        clone: () => ({
+          json: () => Promise.resolve(),
+        }),
+        status: 200,
+      };
+      mockHttpClient.post.mockResolvedValue(httpResponse);
+
+      const resp = await management.sso.configureOIDCSettings(
+        't1',
+        {
+          clientId: 'cid',
+          name: 'cn',
+          attributeMapping: {
+            email: 'em',
+          },
+          redirectUrl: 'http://redirect.com',
+        },
+        ['a.com', 'b.com'],
+        'somessoid',
+      );
+
+      expect(mockHttpClient.post).toHaveBeenCalledWith(
+        apiPaths.sso.oidc.configure,
+        {
+          tenantId: 't1',
+          ssoId: 'somessoid',
+          settings: {
+            clientId: 'cid',
+            name: 'cn',
+            userAttrMapping: {
+              email: 'em',
+            },
+            redirectUrl: 'http://redirect.com',
+          },
+          domains: ['a.com', 'b.com'],
+        },
+        { token: 'key' },
+      );
+
+      expect(resp).toEqual({
+        code: 200,
+        ok: true,
+        response: httpResponse,
+      });
+    });
   });
 
   describe('configureSAMLSettings', () => {
@@ -267,6 +344,55 @@ describe('Management SSO', () => {
         apiPaths.sso.saml.configure,
         {
           tenantId: 't1',
+          settings: {
+            idpUrl: 'https://idp.url',
+            entityId: 'eid',
+            idpCert: 'bsae64cert',
+            spACSUrl: 'https://spacs.url',
+            spEntityId: 'spentityid',
+          },
+          redirectUrl: 'http://redirect.com',
+          domains: ['a.com', 'b.com'],
+        },
+        { token: 'key' },
+      );
+
+      expect(resp).toEqual({
+        code: 200,
+        ok: true,
+        response: httpResponse,
+      });
+    });
+
+    it('should send the correct request and receive correct response with sso id', async () => {
+      const httpResponse = {
+        ok: true,
+        clone: () => ({
+          json: () => Promise.resolve(),
+        }),
+        status: 200,
+      };
+      mockHttpClient.post.mockResolvedValue(httpResponse);
+
+      const resp = await management.sso.configureSAMLSettings(
+        't1',
+        {
+          idpUrl: 'https://idp.url',
+          entityId: 'eid',
+          idpCert: 'bsae64cert',
+          spACSUrl: 'https://spacs.url',
+          spEntityId: 'spentityid',
+        },
+        'http://redirect.com',
+        ['a.com', 'b.com'],
+        'somessoid',
+      );
+
+      expect(mockHttpClient.post).toHaveBeenCalledWith(
+        apiPaths.sso.saml.configure,
+        {
+          tenantId: 't1',
+          ssoId: 'somessoid',
           settings: {
             idpUrl: 'https://idp.url',
             entityId: 'eid',
@@ -333,6 +459,112 @@ describe('Management SSO', () => {
         response: httpResponse,
       });
     });
+
+    it('should send the correct request and receive correct response with sso id', async () => {
+      const httpResponse = {
+        ok: true,
+        clone: () => ({
+          json: () => Promise.resolve(),
+        }),
+        status: 200,
+      };
+      mockHttpClient.post.mockResolvedValue(httpResponse);
+
+      const resp = await management.sso.configureSAMLByMetadata(
+        't1',
+        {
+          idpMetadataUrl: 'https://metadata.com',
+          attributeMapping: { name: 'IDP_NAME', email: 'IDP_MAIL' },
+          spACSUrl: 'https://spacs.url',
+          spEntityId: 'spentityid',
+        },
+        'http://redirect.com',
+        ['a.com', 'b.com'],
+        'somessoid',
+      );
+
+      expect(mockHttpClient.post).toHaveBeenCalledWith(
+        apiPaths.sso.saml.metadata,
+        {
+          tenantId: 't1',
+          ssoId: 'somessoid',
+          settings: {
+            idpMetadataUrl: 'https://metadata.com',
+            attributeMapping: { name: 'IDP_NAME', email: 'IDP_MAIL' },
+            spACSUrl: 'https://spacs.url',
+            spEntityId: 'spentityid',
+          },
+          redirectUrl: 'http://redirect.com',
+          domains: ['a.com', 'b.com'],
+        },
+        { token: 'key' },
+      );
+
+      expect(resp).toEqual({
+        code: 200,
+        ok: true,
+        response: httpResponse,
+      });
+    });
+  });
+
+  describe('newSettings', () => {
+    it('should send the correct request and receive correct response', async () => {
+      const mockResponse = {
+        tenant: {
+          id: 't1',
+          name: 'nm',
+        },
+        oidc: {
+          userAttrMapping: {
+            name: 'uan',
+          },
+        },
+        saml: {
+          groupsMapping: [{ groups: ['g1', 'g2'], role: { id: 'rid', name: 'rname' } }],
+        },
+      };
+      const httpResponse = {
+        ok: true,
+        json: () => mockResponse,
+        clone: () => ({
+          json: () => Promise.resolve(mockResponse),
+        }),
+        status: 200,
+      };
+      mockHttpClient.post.mockResolvedValue(httpResponse);
+      const resp = await management.sso.newSettings('t1', 'somessoid', 'somessodisplayname');
+
+      expect(mockHttpClient.post).toHaveBeenCalledWith(
+        apiPaths.sso.settingsNew,
+        {
+          tenantId: 't1',
+          ssoId: 'somessoid',
+          displayName: 'somessodisplayname',
+        },
+        { token: 'key' },
+      );
+
+      expect(resp).toEqual({
+        code: 200,
+        ok: true,
+        response: httpResponse,
+        data: {
+          tenant: {
+            id: 't1',
+            name: 'nm',
+          },
+          oidc: {
+            attributeMapping: {
+              name: 'uan',
+            },
+          },
+          saml: {
+            groupsMapping: [{ groups: ['g1', 'g2'], roleName: 'rname' }],
+          },
+        },
+      });
+    });
   });
 
   describe('loadSettings', () => {
@@ -387,6 +619,123 @@ describe('Management SSO', () => {
             groupsMapping: [{ groups: ['g1', 'g2'], roleName: 'rname' }],
           },
         },
+      });
+    });
+
+    it('should send the correct request and receive correct response with sso id', async () => {
+      const mockResponse = {
+        tenant: {
+          id: 't1',
+          name: 'nm',
+        },
+        oidc: {
+          userAttrMapping: {
+            name: 'uan',
+          },
+        },
+        saml: {
+          groupsMapping: [{ groups: ['g1', 'g2'], role: { id: 'rid', name: 'rname' } }],
+        },
+      };
+      const httpResponse = {
+        ok: true,
+        json: () => mockResponse,
+        clone: () => ({
+          json: () => Promise.resolve(mockResponse),
+        }),
+        status: 200,
+      };
+      mockHttpClient.get.mockResolvedValue(httpResponse);
+      const resp = await management.sso.loadSettings('t1', 'somessoid');
+
+      expect(mockHttpClient.get).toHaveBeenCalledWith(apiPaths.sso.settingsv2, {
+        queryParams: {
+          tenantId: 't1',
+          ssoId: 'somessoid',
+        },
+        token: 'key',
+      });
+
+      expect(resp).toEqual({
+        code: 200,
+        ok: true,
+        response: httpResponse,
+        data: {
+          tenant: {
+            id: 't1',
+            name: 'nm',
+          },
+          oidc: {
+            attributeMapping: {
+              name: 'uan',
+            },
+          },
+          saml: {
+            groupsMapping: [{ groups: ['g1', 'g2'], roleName: 'rname' }],
+          },
+        },
+      });
+    });
+  });
+
+  describe('loadAllSettings', () => {
+    it('should send the correct request and receive correct response', async () => {
+      const mockResponse = [
+        {
+          tenant: {
+            id: 't1',
+            name: 'nm',
+          },
+          oidc: {
+            userAttrMapping: {
+              name: 'uan',
+            },
+          },
+          saml: {
+            groupsMapping: [{ groups: ['g1', 'g2'], role: { id: 'rid', name: 'rname' } }],
+          },
+          ssoId: 'somessoid',
+        },
+      ];
+      const httpResponse = {
+        ok: true,
+        json: () => mockResponse,
+        clone: () => ({
+          json: () => Promise.resolve(mockResponse),
+        }),
+        status: 200,
+      };
+      mockHttpClient.get.mockResolvedValue(httpResponse);
+      const resp = await management.sso.loadAllSettings('t1');
+
+      expect(mockHttpClient.get).toHaveBeenCalledWith(apiPaths.sso.settingsAllV2, {
+        queryParams: {
+          tenantId: 't1',
+        },
+        token: 'key',
+      });
+
+      expect(resp).toEqual({
+        code: 200,
+        ok: true,
+        response: httpResponse,
+        data: [
+          {
+            tenant: {
+              id: 't1',
+              name: 'nm',
+            },
+            oidc: {
+              attributeMapping: {
+                name: 'uan',
+              },
+            },
+            saml: {
+              groupsMapping: [{ groups: ['g1', 'g2'], roleName: 'rname' }],
+            },
+            ssoId: 'somessoid',
+          },
+        ],
       });
     });
   });
