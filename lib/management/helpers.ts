01852c04fc34ef0a1a6d7f646e0592133b425a87
Asaf Shen <asaf@descope.com>
2025-07-15 12:11:39 +0300

    format

--- lib/management/helpers.ts
+++ lib/management/helpers.ts
@@ -5,10 +5,8 @@ import { User } from './types';
  * Transforms user objects by converting roles to roleNames
  */
 export function transformUsersForBatch(users: User[]): any[] {
-  return users.map(({roles, ...user}) => {
-    return {
-      ...user,
-      roleNames: roles,
-    };
-  });
+  return users.map(({ roles, ...user }) => ({
+    ...user,
+    roleNames: roles,
+  }));
 }
